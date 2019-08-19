1d26fa946a037452e88116528ff74749e89ca39c
Eric Gjertsen <ericgj72@gmail.com>
2019-08-19 10:50:02 -0400

    add test-function command, add python-test image prebuilt with tox, pytest
    Note: pytest installed externally doesn't work right with libraries like
    hypothesis that it has some kind of plugin for. Also I want to see if docker
    based testing works as well as tox, as it certainly would be cleaner.
    
    minor updates to other commands: kms, pip-lock

---
+++ functions/fetch/test/unit/unit/test_unit_article_fetch.py
@@ -0,0 +1,11 @@
+from hypothesis import given, settings
+
+from model.article import Article
+from test.unit.util.fetch import request_examples
+
+
+@given(request=request_examples())
+@settings(deadline=None)
+def test_(request):
+    _ = Article.fetch(request)
+    assert True
